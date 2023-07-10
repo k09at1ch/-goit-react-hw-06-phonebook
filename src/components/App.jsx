becde86f3e6a24c26931d4b123663fc8c1f4422b
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-07-10 21:15:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import style from './Class.module.css';
 import ContactForm from './contactForm/ContactForm';
 import ContactList from './contactList/ContactList';
 import Filter from './filter/Filter';
+  
 
 function App() {
   return (
